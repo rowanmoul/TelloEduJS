2442cfcfe6d4b3afba7581070e9a6b4d67b56c6a
Rowan Moul <rowanmoul@users.noreply.github.com>
2019-02-02 17:30:20 -0700

    Refactor Drone constructor tests to be more descriptive.

--- test/DroneTests.js
+++ test/DroneTests.js
@@ -1,20 +1,60 @@
 const expect = require("chai").expect;
 const Drone = require("../src/Drone");
 
-describe("Drone", function() {
-    describe("Constructor", function() {
-        it("should produce a Drone instance with expected properties and methods", function() {
+describe("Drone Class", function() {
+    context("Constructor with no arguments", function() {
+        it("should produce a Drone instance with an undefined ip address.", function() {
             const drone = new Drone();
             expect(drone).to.have.property("ipAddress").that.is.undefined;
+        });
+
+        it("should produce a Drone instance with method 'activateSDKMode'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("activateSDKMode").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'setWirelessCredentials'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("setWirelessCredentials").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'connectToExternalNetwork'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("connectToExternalNetwork").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getCommandSocket'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getCommandSocket").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getStateSocket'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getStateSocket").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getVideoStreamSocket'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getVideoStreamSocket").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getSpeed'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getSpeed").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getBatteryLevel'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getBatteryLevel").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getFlightTime'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getFlightTime").that.is.a("function");
+        });
+
+        it("should produce a Drone instance with method 'getWirelessSettings'", function() {
+            const drone = new Drone();
             expect(drone).to.have.property("getWirelessSettings").that.is.a("function");
         });
     });
